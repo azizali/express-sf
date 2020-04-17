d637501ec2a5f31562a71bca2895c14eac3d1883
Aziz Ali <aziz@ilovecoding.org>
2020-04-17 15:13:10 -0500

    Express post middleware, body parser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
 
+app.listen(2222, ()=>{
+  console.log('App is being served');
+})
 
+app.use(bodyParser.urlencoded({ extended: true }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
-console.log('Hello');
+app.get('/',
+  (req, res, next)=>{
+    console.log('First middleware');
+    const isAuth = false;
+    if(isAuth){
+      return next()
+    }
+    return res.send('You are not authorized')
+  },
+  (req, res)=>{
+    console.log('Second middleware');
+    res.send('Hello world')
+  }
+);
+
+app.get('/user',
+  (req, res, next)=>{
+    console.log('First user middleware');
+    res.send('You are on /user route');
+    next();
+  }
+);
+
+app.post('/user',
+  (req, res, next)=>{
+    console.log(req.body.name, req.body.location);
+    res.send('Saving a new user');
+  }
+);
+
+app.get('/user/:id',
+  (req, res, next)=>{
+    res.send(`You are user ${req.params.id}`);
+    next();
+  }
+);
+
+app.use((req, res, next)=>{
+  console.log(req.body, req.params, req.query)
+  console.log('use middleware');
+  next();
+})
